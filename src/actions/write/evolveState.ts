af93419574c59e8ec00aa49fafa4ab704e330c60
dtfiedler <dylan@ar.io>
2024-04-05 11:57:46 -0500

    chore: fix the end time for gateways leaving

--- src/actions/write/evolveState.ts
+++ src/actions/write/evolveState.ts
@@ -16,10 +16,10 @@ export const evolveState = async (
     if (gateway.status === 'leaving') {
       // fix the expiration to 21 days (was set at 90)
       const currentEndFor90Days = gateway.end;
-      const correctEndFor21Days = currentEndFor90Days - 69 * BLOCKS_PER_DAY;
+      const correctEndFor21Days =
+        currentEndFor90Days - 69 * BLOCKS_PER_DAY + 4 * BLOCKS_PER_DAY; // add 4 days given the mistake
       state.gateways[address].end = correctEndFor21Days;
-      state.gateways[address].vaults[address].end =
-        correctEndFor21Days + 4 * BLOCKS_PER_DAY; // add 4 days given the mistake
+      state.gateways[address].vaults[address].end = correctEndFor21Days;
     }
   }
 
